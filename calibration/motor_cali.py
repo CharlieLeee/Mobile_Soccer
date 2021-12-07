78ea13be4f0e0816e77cc352d2deb7e79e47b1c9
JiangfanLi <rdemezerl@gmail.com>
2021-12-07 16:17:03 +0100

    negative speed

--- calibration/motor_cali.py
+++ calibration/motor_cali.py
@@ -16,12 +16,16 @@ def transitional_vel(speed = 100, span = 5):
     print(F"Speed: {speed}, Timespan: {span}")
 
 def rotational_vel(dir="left", speed = 100, omega = 50, span = 5):
+    ls = speed - omega
+    rs = speed + omega
+    ls = ls if ls > 0 else 2 ** 16 + ls
+    rs = rs if rs > 0 else 2 ** 16 + rs
     if dir == "left":
-        th.set_var("motor.left.target", speed - omega)
-        th.set_var("motor.right.target", speed + omega)
+        th.set_var("motor.left.target", ls)
+        th.set_var("motor.right.target", rs)
     else:
-        th.set_var("motor.left.target", speed + omega)
-        th.set_var("motor.right.target", speed - omega)
+        th.set_var("motor.left.target", rs)
+        th.set_var("motor.right.target", ls)
     starter = time.time()
     while time.time() - starter < span:
         pass
@@ -36,11 +40,11 @@ with Thymio.serial(port=PORT, refreshing_rate=0.1) as th:
     while True:
         try:
             if th["button.forward"]:
-                transitional_vel(speed=500, span=2)
+                transitional_vel(speed=100, span=10)
             elif th["button.left"]:
-                rotational_vel("left",speed=100, omega=20, span=5)
+                rotational_vel("left",speed=0, omega=91, span=5)
             elif th["button.right"]:
-                rotational_vel("right",speed=100, omega=20, span=5)
+                rotational_vel("right",speed=0, omega=-91, span=5)
             elif th["button.center"]:
                 stop()
                 th.close()
